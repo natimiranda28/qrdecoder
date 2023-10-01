b29ddb648075de83eabcfc0c7f5ab82cce13635a
Natalia <natumiranda.28@gmail.com>
2023-10-01 15:27:39 -0300

    Actualizar qrdecode.py

--- qrdecode.py
+++ qrdecode.py
@@ -1,4 +1,22 @@
 import qrcode
+from pyzbar.pyzbar import decode
+from PIL import Image
 
-data = "https://www.linkedin.com/in/maria-natalia-miranda"
-img = qrcode.make(data)
+# Generar un código QR
+data = "https://www.ejemplo.com"
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("codigo_qr.png")
+
+# Leer un código QR desde una imagen
+qr_code = Image.open("codigo_qr.png")
+decoded_data = decode(qr_code)
+print(decoded_data[0].data.decode('utf-8'))
